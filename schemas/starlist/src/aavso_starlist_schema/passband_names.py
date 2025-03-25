75739cdc30061857caeff25f4b5636a6ab821b86
Matt Craig <mattwcraig@gmail.com>
2025-03-25 09:31:46 -0500

    Add hardcoded passbands in case VSX is down
    Otherwise the code cannot even be imported....

--- schemas/starlist/src/aavso_starlist_schema/passband_names.py
+++ schemas/starlist/src/aavso_starlist_schema/passband_names.py
@@ -5,6 +5,57 @@ import requests
 PASSBAND_URL = "https://www.aavso.org/vsx/index.php?view=api.bands&format=json"
 PASSBAND_VERSION = "2"
 
+# Copy/paste from stellarphot
+class _AAVSOFilters(StrEnum):
+    """
+    The definitive list of AAVSO filters is at https://www.aavso.org/filters
+    """
+
+    U = "U"
+    B = "B"
+    V = "V"
+    RJ = "RJ"
+    Rc = "R"
+    Ic = "I"
+    IJ = "IJ"
+    J = "J"
+    H = "H"
+    K = "K"
+    TG = "TG"
+    TB = "TB"
+    TR = "TR"
+    CV = "CV"
+    CR = "CR"
+    SZ = "SZ"
+    SU = "SU"
+    SG = "SG"
+    SR = "SR"
+    SI = "SI"
+    STU = "STU"
+    STV = "STV"
+    STB = "STB"
+    STY = "STY"
+    STHBW = "STHBW"
+    STHBN = "STHBN"
+    MA = "MA"
+    MB = "MB"
+    MI = "MI"
+    ZS = "ZS"
+    Y = "Y"
+    HA = "HA"
+    HAC = "HAC"
+    CBB = "CBB"
+    O = "O"  # noqa: E741
+
+
+
+def _fallback_filters():
+    """
+    Provide filter names in case VSX is down or the request times out.
+    """
+    return [{"ShortName": filter_name} for filter_name in _AAVSOFilters]
+
+
 
 def get_passband_data():
     """
@@ -12,7 +63,12 @@ def get_passband_data():
 
     Note that only the passband data is returned, not the entire response.
     """
-    response = requests.get(PASSBAND_URL)
+    try:
+        response = requests.get(PASSBAND_URL, timeout=10)
+    except requests.ReadTimeout:
+        # Fall back to some hard-coded passband data if the request times out
+        return _fallback_filters()
+
     if response.status_code != 200:
         raise ValueError(
             f"Failed to retrieve passband data from {PASSBAND_URL}:\n"
