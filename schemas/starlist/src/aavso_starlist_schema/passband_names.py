ebc5329990137b49ceb5ffc1d438644909d12742
Matt Craig <mattwcraig@gmail.com>
2025-05-14 10:35:03 -0500

    Change list of passbands that smart telescope may report

--- schemas/starlist/src/aavso_starlist_schema/passband_names.py
+++ schemas/starlist/src/aavso_starlist_schema/passband_names.py
@@ -1,95 +1,11 @@
 from enum import StrEnum
 
-import requests
+__all__ = ["AAVSOFilters"]
 
-PASSBAND_URL = "https://vsx.aavso.org/index.php?view=api.bands&format=json"
-PASSBAND_VERSION = "2"
-
-# Copy/paste from stellarphot
-class _AAVSOFilters(StrEnum):
-    """
-    The definitive list of AAVSO filters is at https://www.aavso.org/filters
-    """
-
-    U = "U"
-    B = "B"
-    V = "V"
-    RJ = "RJ"
-    Rc = "R"
-    Ic = "I"
-    IJ = "IJ"
-    J = "J"
-    H = "H"
-    K = "K"
+# Restricted list that smart telescopes may report
+class AAVSOFilters(StrEnum):
     TG = "TG"
-    TB = "TB"
     TR = "TR"
-    CV = "CV"
-    CR = "CR"
-    SZ = "SZ"
-    SU = "SU"
-    SG = "SG"
-    SR = "SR"
-    SI = "SI"
-    STU = "STU"
-    STV = "STV"
-    STB = "STB"
-    STY = "STY"
-    STHBW = "STHBW"
-    STHBN = "STHBN"
-    MA = "MA"
-    MB = "MB"
-    MI = "MI"
-    ZS = "ZS"
-    Y = "Y"
-    HA = "HA"
-    HAC = "HAC"
-    CBB = "CBB"
-    O = "O"  # noqa: E741
-
-
-
-def _fallback_filters():
-    """
-    Provide filter names in case VSX is down or the request times out.
-    """
-    return [{"ShortName": filter_name} for filter_name in _AAVSOFilters]
-
-
-
-def get_passband_data():
-    """
-    Retrieve the passband data from the AAVSO API and return it as a dictionary.
-
-    Note that only the passband data is returned, not the entire response.
-    """
-    try:
-        response = requests.get(PASSBAND_URL, timeout=10)
-    except requests.ReadTimeout:
-        # Fall back to some hard-coded passband data if the request times out
-        return _fallback_filters()
-
-    if response.status_code != 200:
-        raise ValueError(
-            f"Failed to retrieve passband data from {PASSBAND_URL}:\n"
-            f"{response.text}"
-        )
-    content = response.json()
-    if content["Bands"]["@version"] != PASSBAND_VERSION:
-        raise ValueError(
-            f"Unexpected passband data version. Expected {PASSBAND_VERSION}, "
-            f"but got {content['Bands']['@version']}"
-        )
-
-    return content["Bands"]["Band"]
-
-
-AAVSOFilters = StrEnum(
-    "AAVSOFilters",
-    {
-        # This odd structure ensures that the enum value is the same as the
-        # short filter name.
-        band["ShortName"]: band["ShortName"]
-        for band in get_passband_data()
-    }
-)
+    TB = "TB"
+    L3 = "L3"
+    L4 = "L4"
